da9e17b5b518e1f36650f913a63e5af6e08fdc55
David Tsuji <david@flodge.com>
2014-04-07 09:29:44 +1000

    added .jshintrc, .npmignore and started integrating jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require( 'gulp' ),
+	jshint = require( 'gulp-jshint' ),
+	jshintReporter = require( 'jshint-stylish' ),
 	shell = require( 'gulp-shell' );
 
 var pkg = require( './package.json' );
 
-gulp.task( 'test', function () {
+gulp.task( "jshint", function () {
+	return gulp.src( [ "./src/scripts/**/*.js", "test/**/*.js" ] )
+		.pipe( jshint() )
+		.pipe( jshint.reporter( jshintReporter ) );
+} );
+
+gulp.task( 'test', [ 'jshint' ], function () {
 	return gulp.src( '' ).pipe( shell( [ 'npm test' ] ) );
 } );
 
 gulp.task( 'watch', function () {
-	gulp.watch( [ './src/**/*.js' ], [ 'scripts', 'test' ] );
+	gulp.watch( [ './src/**/*.js' ], [ 'test' ] );
 	gulp.watch( [ './test/**/*' ], [ 'test' ] );
 	gulp.watch( [ '*.js' ], [ 'default' ] );
 } );
