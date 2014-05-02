421148ac6ab97b733f0db9a672d0e983de1d7ade
dervalp <dervalp@gmail.com>
2014-05-02 16:24:04 +0100

    adapterV2

--- src/index.js
+++ src/index.js
@@ -1,38 +1,63 @@
 var request = require( 'superagent' ),
-	cast = require( 'sc-cast' ),
-	is = require( 'sc-is' ),
-	hasKey = require( 'sc-haskey' );
+  cast = require( 'sc-cast' ),
+  is = require( 'sc-is' ),
+  hasKey = require( 'sc-haskey' );
 
-module.exports = function ( _model, _data, _method, _url, _callback ) {
-	var method = /delete/i.test( _method ) ? 'del' : _method,
-		model = _model,
-		data = cast( _data, 'object', {} ),
-		url = _url,
-		error;
+var req = function ( _moldy, _data, _method, _url, _callback ) {
+  var method = /delete/i.test( _method ) ? 'del' : _method,
+    data = cast( _data, 'object', {} ),
+    url = _url,
+    error;
 
-	if ( /get/i.test( method ) && hasKey( data, model.__key ) && new RegExp( data[ model.__key ] ).test( url ) === false ) {
-		url += '/' + data[ model.__key ];
-		delete data[ model.__key ];
-	}
+  if ( /get/i.test( method ) && hasKey( data, _moldy.__key ) && new RegExp( data[ _moldy.__key ] ).test( url ) === false ) {
+    url += '/' + data[ _moldy.__key ];
+    delete data[ _moldy.__key ];
+  }
 
-	request[ method ]( url )[ /get/i.test( method ) ? 'query' : 'send' ]( data )
-		.set( model.$headers() )
-		.type( 'json' )
-		.accept( 'json' )
-		.end( function ( _error, _res ) {
-			var res = is.an.object( _res ) ? _res : {},
-				body = hasKey( _res, 'body' ) && ( is.object( _res.body ) || is.array( _res.body ) ) ? _res.body : null;
 
-			if ( res[ 'ok' ] !== true ) {
-				error = new Error( 'The response from the server was not OK' );
-			}
+  request[ method ]( url )[ /get/i.test( method ) ? 'query' : 'send' ]( data )
+    .set( _moldy.$headers() )
+    .type( 'json' )
+    .accept( 'json' )
+    .end( function ( _error, _res ) {
+      var res = is.an.object( _res ) ? _res : {},
+        body = hasKey( _res, 'body' ) && ( is.object( _res.body ) || is.array( _res.body ) ) ? _res.body : null;
 
-			if ( body === null ) {
-				error = new Error( 'The response from the server contained an empty body' );
-			}
+      if ( res.ok !== true ) {
+        error = new Error( 'The response from the server was not OK' );
+      }
 
-			_callback && _callback( error, body );
+      if ( body === null ) {
+        error = new Error( 'The response from the server contained an empty body' );
+      }
 
-		} );
+      if ( _callback ) {
+        _callback( error, body );
+      }
 
-}
+    } );
+
+};
+
+module.exports = {
+  name: "ajax",
+  create: function ( data, done ) {
+    req( this, data, 'post', this.$url(), done );
+  },
+  findOne: function ( query, done ) {
+    req( this, query, 'get', this.$url(), done );
+  },
+  find: function ( query, done ) {
+    req( this, query, 'get', this.$url(), done );
+  },
+  save: function ( data, done ) {
+    var url = this.$url() + ( this.__keyless ? '' : '/' + data[ this.__key ] );
+
+    req( this, data, 'put', url, done );
+  },
+  destroy: function ( data, done ) {
+    var url = this.$url() + ( this.__keyless ? '' : '/' + data[ this.__key ] );
+
+    req( this, data, 'delete', url, done );
+  }
+};
