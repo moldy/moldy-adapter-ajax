05f61e13fccedc501c3f4f2302862dd3a4e4d59e
David Tsuji <david@flodge.com>
2014-05-04 07:25:02 +1000

    jsformat

--- src/index.js
+++ src/index.js
@@ -1,64 +1,64 @@
 var request = require( 'superagent' ),
-  cast = require( 'sc-cast' ),
-  baseAdapter = require( 'moldy-base-adapter' ),
-  is = require( 'sc-is' ),
-  hasKey = require( 'sc-haskey' );
+	cast = require( 'sc-cast' ),
+	baseAdapter = require( 'moldy-base-adapter' ),
+	is = require( 'sc-is' ),
+	hasKey = require( 'sc-haskey' );
 
 var req = function ( _moldy, _data, _method, _url, _callback ) {
-  var method = /delete/i.test( _method ) ? 'del' : _method,
-    data = cast( _data, 'object', {} ),
-    url = _url,
-    error;
+	var method = /delete/i.test( _method ) ? 'del' : _method,
+		data = cast( _data, 'object', {} ),
+		url = _url,
+		error;
 
-  if ( /get/i.test( method ) && hasKey( data, _moldy.__key ) && new RegExp( data[ _moldy.__key ] ).test( url ) === false ) {
-    url += '/' + data[ _moldy.__key ];
-    delete data[ _moldy.__key ];
-  }
+	if ( /get/i.test( method ) && hasKey( data, _moldy.__key ) && new RegExp( data[ _moldy.__key ] ).test( url ) === false ) {
+		url += '/' + data[ _moldy.__key ];
+		delete data[ _moldy.__key ];
+	}
 
 
-  request[ method ]( url )[ /get/i.test( method ) ? 'query' : 'send' ]( data )
-    .set( _moldy.$headers() )
-    .type( 'json' )
-    .accept( 'json' )
-    .end( function ( _error, _res ) {
-      var res = is.an.object( _res ) ? _res : {},
-        body = hasKey( _res, 'body' ) && ( is.object( _res.body ) || is.array( _res.body ) ) ? _res.body : null;
+	request[ method ]( url )[ /get/i.test( method ) ? 'query' : 'send' ]( data )
+		.set( _moldy.$headers() )
+		.type( 'json' )
+		.accept( 'json' )
+		.end( function ( _error, _res ) {
+			var res = is.an.object( _res ) ? _res : {},
+				body = hasKey( _res, 'body' ) && ( is.object( _res.body ) || is.array( _res.body ) ) ? _res.body : null;
 
-      if ( res.ok !== true ) {
-        error = new Error( 'The response from the server was not OK' );
-      }
+			if ( res.ok !== true ) {
+				error = new Error( 'The response from the server was not OK' );
+			}
 
-      if ( body === null ) {
-        error = new Error( 'The response from the server contained an empty body' );
-      }
+			if ( body === null ) {
+				error = new Error( 'The response from the server contained an empty body' );
+			}
 
-      if ( _callback ) {
-        _callback( error, body );
-      }
+			if ( _callback ) {
+				_callback( error, body );
+			}
 
-    } );
+		} );
 
 };
 
-module.exports = baseAdapter.extend({
-  name: "ajax",
-  create: function ( data, done ) {
-    req( this, data, 'post', this.$url(), done );
-  },
-  findOne: function ( query, done ) {
-    req( this, query, 'get', this.$url(), done );
-  },
-  find: function ( query, done ) {
-    req( this, query, 'get', this.$url(), done );
-  },
-  save: function ( data, done ) {
-    var url = this.$url() + ( this.__keyless ? '' : '/' + data[ this.__key ] );
+module.exports = baseAdapter.extend( {
+	name: "ajax",
+	create: function ( data, done ) {
+		req( this, data, 'post', this.$url(), done );
+	},
+	findOne: function ( query, done ) {
+		req( this, query, 'get', this.$url(), done );
+	},
+	find: function ( query, done ) {
+		req( this, query, 'get', this.$url(), done );
+	},
+	save: function ( data, done ) {
+		var url = this.$url() + ( this.__keyless ? '' : '/' + data[ this.__key ] );
 
-    req( this, data, 'put', url, done );
-  },
-  destroy: function ( data, done ) {
-    var url = this.$url() + ( this.__keyless ? '' : '/' + data[ this.__key ] );
+		req( this, data, 'put', url, done );
+	},
+	destroy: function ( data, done ) {
+		var url = this.$url() + ( this.__keyless ? '' : '/' + data[ this.__key ] );
 
-    req( this, data, 'delete', url, done );
-  }
-});
+		req( this, data, 'delete', url, done );
+	}
+} );
--- test/readme.js
+++ test/readme.js
@@ -1,18 +1,18 @@
 var Moldy = require( 'moldy' ),
-  should = require( 'should' );
+	should = require( 'should' );
 
 Moldy.use( require( '../src' ) );
 
 describe( 'model-ajax-adapter', function () {
 
-  it( 'Tell the `Moldy` to use the `ajax` adapter', function () {
-    // Moldy.use( require('moldy-ajax-adapter') );
-  } );
+	it( 'Tell the `Moldy` to use the `ajax` adapter', function () {
+		// Moldy.use( require('moldy-ajax-adapter') );
+	} );
 
-  require( './readme/create' );
-  require( './readme/findOne' );
-  require( './readme/find' );
-  require( './readme/save' );
-  require( './readme/destroy' );
+	require( './readme/create' );
+	require( './readme/findOne' );
+	require( './readme/find' );
+	require( './readme/save' );
+	require( './readme/destroy' );
 
 } );
